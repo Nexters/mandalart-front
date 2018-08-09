a9a6f991b815b6a0e1dfa87bcbd984bf86bf0845
SeongkyuLee <seongkyu0617@gmail.com>
2018-08-10 03:26:02 +0900

    feat: make landing container and component

---
+++ src/containers/LandingContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Landing from '../components/Landing/Landing';
+
+class LandingContainer extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div> <Landing /> </div>
+        )
+    }
+}
+
+export default LandingContainer;
