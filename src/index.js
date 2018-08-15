1157d8164e26fdefefc0ec905b4defa317866afd
heeinso <heeinso@snu.ac.kr>
2018-08-15 19:29:46 +0900

    Connect Local State to Components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 
 import client from './apolloConfig';
-import App from './main/App';
+import App from './components/App';
 
 ReactModal.setAppElement('#root');
 
